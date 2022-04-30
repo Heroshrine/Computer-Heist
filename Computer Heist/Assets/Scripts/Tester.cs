9793c95e1b066772da664a3d8d182367058d080a
Heroshrine <39038511+Heroshrine@users.noreply.github.com>
2022-04-29 19:53:51 -0700

    Lattice2D script and most of PathNode complete

--- Computer Heist/Assets/Scripts/Tester.cs
+++ Computer Heist/Assets/Scripts/Tester.cs
@@ -7,14 +7,46 @@ public class Tester : MonoBehaviour
 {
     void Start()
     {
+        /*/
         Debug.Log("constructor 1: ");
-        Grid<int> grid1 = new Grid<int>(4, 4);
-        foreach (int i in grid1)
+        Lattice2D<PathNode> lattice = new Lattice2D<PathNode>(4, 4, new Vector2(-6, 12));
+
+        for (int x = 0; x < lattice.Width; x++)
         {
-            Debug.Log(i);
+            for (int y = 0; y < lattice.Height; y++)
+            {
+                lattice[x, y] = new PathNode(lattice, x, y);
+                lattice[x, y].GCost = PathNode.VERY_LARGE;
+            }
         }
 
+        foreach (PathNode i in lattice)
+        {
+            Debug.Log(i.Position);
+        }
+        /*/
 
+        Lattice2D<GameObject> gameObjectLattice = new Lattice2D<GameObject>(6, 6, new Vector2(0, 0));
 
+        for (int x = 0; x < gameObjectLattice.Width; x++)
+        {
+            for (int y = 0; y < gameObjectLattice.Height; y++)
+            {
+
+                gameObjectLattice[x, y] = new GameObject();
+                gameObjectLattice[x, y].transform.position = gameObjectLattice.LatticeToWorldPoint(x, y);
+                gameObjectLattice[x, y].AddComponent<Debugger>();
+
+            }
+        }
+    }
+}
+
+public class Debugger : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 0.35f);
     }
 }
